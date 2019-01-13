8a25873d34e1eb1a415b77cd10f6dbbb83f77692
jygibson <jygibson922@gmail.com>
2019-01-13 16:15:37 -0500

    broke everything with react-bootstrap, then fixed it

--- src/App.js
+++ src/App.js
@@ -2,29 +2,29 @@ import React, { Component } from 'react';
 import SanrioCard from "./components/CardTile";
 import Wrapper from "./components/CardWrapper";
 import Jumbotron from "./components/Jumbotron";
-import Nav from "./components/NavBar";
+import Navbar from "./components/NavBar";
 import kawaii from "./kawaii.json";
 import './App.css';
 
 class App extends Component {
 
-state={
-  kawaii
-}
+  state = {
+    kawaii
+  }
 
   render() {
     return (
       <div>
-        <Nav />
+        <Navbar />
         <Jumbotron />
-      <Wrapper>
-        {this.state.kawaii.map(sanrio => (
-          <SanrioCard
-          key={sanrio.key}
-          image={sanrio.image}
-          />
-        ))}
-      </Wrapper>
+        <Wrapper>
+          {this.state.kawaii.map(sanrio => (
+            <SanrioCard
+              key={sanrio.key}
+              image={sanrio.image}
+            />
+          ))}
+        </Wrapper>
       </div>
     );
   }
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./style.css";
 
-function Nav() {
+function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-dark">
-      <a className="navbar-brand" href="https://www.sanrio.com/">
+    <ul className="nav">
+      <li className="nav-item"><a className="navbar-brand" href="https://www.sanrio.com/">
       <img id="nav-img" src="https://logos-download.com/wp-content/uploads/2016/06/Sanrio_logo-700x210.png" alt="sanrio logo"/>
       <br></br>
       <p>images property of Sanrio</p>
       </a>
+      </li>
+      <li className="nav-item"><p id="score-text">Score:   </p><span id="score"></span></li>
+      <li><p id="top-score-text"> | Top Score:  </p> <span id="top-score"></span></li>
+    </ul>
     </nav>
   );
 }
 
-export default Nav;
+
+export default Navbar;
