55b27927e7a26c8cd134d567c34c0a701330691e
jygibson <jygibson922@gmail.com>
2019-01-15 13:00:23 -0500

    Updates

---
+++ precache-manifest.b898f68982df88eab0ab2f5cbba53ef7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "4d4027736456fb27f746",
+    "url": "/clicky-game/static/js/main.4d402773.chunk.js"
+  },
+  {
+    "revision": "b05f58ae460c5f47cea3",
+    "url": "/clicky-game/static/js/1.b05f58ae.chunk.js"
+  },
+  {
+    "revision": "4d4027736456fb27f746",
+    "url": "/clicky-game/static/css/main.0ad19ba6.chunk.css"
+  },
+  {
+    "revision": "c3f94efc5ece993e671dda2eb420afbf",
+    "url": "/clicky-game/index.html"
+  }
+];
